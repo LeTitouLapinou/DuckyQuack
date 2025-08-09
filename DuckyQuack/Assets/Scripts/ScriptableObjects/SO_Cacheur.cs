3bd27b66be08d2001d39724efcbd19fc944b4f06
LeTitouLapinou <162844593+LeTitouLapinou@users.noreply.github.com>
2025-08-09 10:40:55 -0400

    Duckling  manager inside controller

---
+++ DuckyQuack/Assets/Scripts/ScriptableObjects/SO_Cacheur.cs
@@ -0,0 +1,14 @@
+using EditorAttributes;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_Cacheur", menuName = "GPI/SO_Cacheur")]
+public class SO_Cacheur : ScriptableObject
+{
+    public Mesh mesh;
+
+    public Material initialMaterial;
+    public Material swappedMaterial;
+
+    [Title("Number of ducklings")]
+    public int ducklingNumber;
+}
